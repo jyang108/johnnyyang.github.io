56b7873393c27410d0896f046a691d8cf3f78262
jyang108 <jyang108@gmail.com>
2019-10-19 23:39:54 -0500

    fixed timeslot typo

--- homework-5/script.js
+++ homework-5/script.js
@@ -10,7 +10,7 @@ $("#currentTime").text(currentTime)
 console.log(currentHour);
 
 //set variables for time and hour
-var time = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 AM"];
+var time = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 //hour used for comparing currentHour to div for styling
 var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
